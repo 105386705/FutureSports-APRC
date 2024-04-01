4696fb5521d2dd83c260a0e8c388acbf676fd586
Luke Walters <krypticquasar@gmail.com>
2024-04-01 11:50:00 +1100

    Compile error fix

--- my-app/src/components/MainContent/MapFrame.js
+++ my-app/src/components/MainContent/MapFrame.js
@@ -3,6 +3,7 @@ import React from 'react';
 const MapFrame = () => (
   <section className="map">
     <iframe
+      title="Google Maps Embed"
       src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12602.02009354019!2d144.96611302953212!3d-37.84847211019015!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ad667e1dead25bd%3A0x5045675218ce8a0!2sAlbert%20Park%20VIC%203206!5e0!3m2!1sen!2sau!4v1709564506954!5m2!1sen!2sau"
       width="600"
       height="450"
